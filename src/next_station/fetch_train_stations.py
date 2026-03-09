ab73c1e42b9988b1453366cd4bc870e0fb7d9081
Luki <lukasjagusz6@gmail.com>
2026-03-09 17:22:44 +0100

    create function to download coordinates of train stations

---
+++ src/next_station/fetch_train_stations.py
@@ -0,0 +1,12 @@
+import requests
+
+def fetch_train_stations(api_url: str,
+                         query: str
+                         ) -> dict | None:
+
+    response = runner(api_url, 'post', query)
+
+    if response is not None:
+        return response.json()
+
+    return None
