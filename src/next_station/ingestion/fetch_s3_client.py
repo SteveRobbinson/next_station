df13b161a516dcdc3558d495d14321760ae78c33
Luki <lukasjagusz6@gmail.com>
2026-03-15 16:32:24 +0100

    replace botocore exceptions with custom s3 errors

--- src/next_station/ingestion/fetch_s3_client.py
+++ src/next_station/ingestion/fetch_s3_client.py
@@ -1,5 +1,6 @@
 import boto3
 from botocore.exceptions import ClientError, NoCredentialsError, PartialCredentialsError, NoRegionError, EndpointConnectionError
+from exceptions import S3ServiceError, S3ConfigError, S3ConnectionError, S3AccessDeniedError, S3NotFoundError
 
 def fetch_s3_client(bucket_name: str,
                     file_name_on_s3: str
@@ -22,19 +23,19 @@ def fetch_s3_client(bucket_name: str,
         status_code = ce.response['Error']['Code']
         
         if status_code == '403':
-            raise ClientError(f"AWS S3 - Access denied. Status code: {status_code}\nProvided credentials do not have permissions to access S3 resources") from ce
+            raise S3AccessDeniedError(f"AWS S3 - Access denied. Status code: {status_code}\nProvided credentials do not have permissions to access S3 resources") from ce
 
         else:
-            raise ClientError(f"AWS S3 - Client Error occured! Status code: {status_code}") from ce
+            raise S3ServiceError(f"AWS S3 - Client Error occurred! Status code: {status_code}") from ce
 
 
     except (NoCredentialsError, PartialCredentialsError) as nce:
-        raise NoCredentialsError(f"AWS S3 - No credentials found. Check your AWS_ACCESS_KEY_ID and/or AWS_SECRET_ACCESS_KEY") from nce
+        raise S3ConfigError(f"AWS S3 - No credentials found. Check your AWS_ACCESS_KEY_ID and/or AWS_SECRET_ACCESS_KEY") from nce
 
 
     except NoRegionError as nre:
-        raise NoRegionError(f"AWS S3 - Region for client S3 not specified!") from nre
+        raise S3ConfigError(f"AWS S3 - Region for client S3 not specified!") from nre
 
 
     except EndpointConnectionError as ece:
-        raise EndpointConnectionError(f"AWS S3 - Could not connect to the endpoint. Please check your internet or AWS service status") from ece
+        raise S3ConnectionError(f"AWS S3 - Could not connect to the endpoint. Please check your internet or AWS service status") from ece
