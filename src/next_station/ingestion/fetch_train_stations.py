2e001e0c6d14e38b28462d3eb78c4d177b9287d2
Luki <lukasjagusz6@gmail.com>
2026-03-15 08:56:23 +0100

    simplify fetch_train_stations by leveraging runner's exception handling

--- src/next_station/ingestion/fetch_train_stations.py
+++ src/next_station/ingestion/fetch_train_stations.py
@@ -1,13 +1,7 @@
-import requests
 from .runner import runner
 
 def fetch_train_stations(api_url: str,
                          query: str
-                         ) -> dict | None:
+                         ) -> dict:
 
-    response = runner(api_url, 'post', query)
-
-    if response is not None:
-        return response.json()
-
-    return None
+    return runner(api_url, 'post', query=query).json()
