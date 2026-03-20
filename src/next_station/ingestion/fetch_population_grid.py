06b127522b5138c6aca7dc223cb6c184aa4c66a2
Luki <lukasjagusz6@gmail.com>
2026-03-20 18:47:00 +0100

    simplify fetch_population_grid by removing metadata check

--- src/next_station/ingestion/fetch_population_grid.py
+++ src/next_station/ingestion/fetch_population_grid.py
@@ -1,9 +1,6 @@
 from .runner import runner
 import requests
 
-def fetch_population_grid(api_url: str,
-                          is_metadata_same: bool
-                          ) -> requests.Response | None:
+def fetch_population_grid(api_url: str) -> requests.Response:
 
-    if not is_metadata_same:
-        return runner(api_url, 'get', stream=True)
+    return runner(api_url, 'get', stream=True)
