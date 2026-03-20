19a8c0e71911d6dc2056740db57d4884d876a01d
Luki <lukasjagusz6@gmail.com>
2026-03-20 12:20:15 +0100

    make metadata argument optional in upload_data_to_s3

--- src/next_station/ingestion/upload_data_to_s3.py
+++ src/next_station/ingestion/upload_data_to_s3.py
@@ -3,12 +3,13 @@ from schemas.worldpop import ApiMetadata
 
 def upload_data_to_s3(bucket_name: str,
                       file_name: str,
-                      object_to_upload: requests.Resonse
+                      object_to_upload: requests.Response,
+                      metadata: None | dict = None
                       ) -> bool:
 
     s3.upload_fileobj(Bucket = bucket_name,
                       Fileobj = object_to_upload.raw,
                       Key = file_name,
-                      Metadata = {'ETag': ApiMetadata(**object_to_upload.headers).api_metadata})
+                      Metadata = metadata)
 
     return True
