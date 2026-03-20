e22cbec03808963895aa37aaa52bb7687e9dc539
Luki <lukasjagusz6@gmail.com>
2026-03-20 14:34:22 +0100

    correct S3 metadata handling and add absolute imports

--- src/next_station/ingestion/upload_data_to_s3.py
+++ src/next_station/ingestion/upload_data_to_s3.py
@@ -1,15 +1,17 @@
 import requests
-from schemas.worldpop import ApiMetadata
+from src.next_station.schemas.worldpop import ApiMetadata
 
 def upload_data_to_s3(bucket_name: str,
                       file_name: str,
                       object_to_upload: requests.Response,
-                      metadata: None | dict = None
+                      metadata: dict | None = None
                       ) -> bool:
+
+    extra_args = {'Metadata': metadata} if metadata else {}
 
     s3.upload_fileobj(Bucket = bucket_name,
                       Fileobj = object_to_upload.raw,
                       Key = file_name,
-                      Metadata = metadata)
+                      ExtraArgs = extra_args)
 
     return True
