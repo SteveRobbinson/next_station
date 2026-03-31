51f4d652c36a5e00901e2fbf9e12af4f88114015
Luki <lukasjagusz6@gmail.com>
2026-03-31 13:52:10 +0200

    create railway stations pipeline from s3 to databricks

---
+++ src/next_station/jobs/02_s3_to_bronze_railway_stations.py
@@ -0,0 +1,24 @@
+from src.next_station.core.spark import get_spark_session
+from src.next_station.infrastructure.load_railway_stations import load_json_source
+from src.next_station.quality.df_empty import is_df_empty
+from src.next_station.quality.melt_table import melt_table
+from src.next_station.core.config import settings
+from src.next_station.infrastructure.databricks import save_df_in_db
+
+spark_session = get_spark_session()
+
+df = load_json_source(spark_session,
+                      (settings.aws_s3_bucket_address,
+                       settings.aws_railway_stations_file_name))
+
+df = melt_table(df, settings.aws_railway_file_explode_by)
+
+df = is_df_empty(df)
+
+save_df_in_db(df,
+              (settings.databricks_catalog,
+               settings.databricks_schema,
+               settings.databricks_railway_table))
+
+
+
