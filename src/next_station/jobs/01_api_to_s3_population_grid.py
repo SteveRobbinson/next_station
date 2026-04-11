0bff62aeb8b1e3553a367138d0169f8c29640f6c
Luki <lukasjagusz6@gmail.com>
2026-04-11 17:56:07 +0200

    integrate in-memory slicing into dataset update workflow

--- src/next_station/jobs/01_api_to_s3_population_grid.py
+++ src/next_station/jobs/01_api_to_s3_population_grid.py
@@ -11,6 +11,7 @@ from src.next_station.schemas.worldpop import ApiMetadata
 from src.next_station.providers.get_file_url import get_file_url
 from src.next_station.providers.fetch_population_grid import fetch_population_grid
 import logging
+from src.next_station.infrastructure.slice_dataset import slice_dataset
 
 logger = logging.getLogger(__name__)
 
@@ -25,27 +26,27 @@ population_grid_file_url = get_file_url(settings.population_grid_url)
 
 s3_object_metadata = get_s3_object_metadata(s3,
                                             settings.aws_bucket_name,
-                                            settings.aws_grid_file_name)
+                                            settings.aws_population_grid_metadata_file)
 
 is_metadata_same = compare_metadata(s3_object_metadata, population_grid_file_url)
 
 
 if not is_metadata_same:
-    logger.info(f"Metadata mismatch for {settings.aws_grid_file_name}. Downloading new dataset...")
+    logger.info(f"Metadata mismatch for {settings.aws_population_grid_file_name}. Downloading new dataset...")
 
     population_grid = fetch_population_grid(population_grid_file_url)
 
     metadata = {'ETag': ApiMetadata(**population_grid.headers).etag}
+    
+    sliced_dataset = slice_dataset(population_grid)
 
     upload_data_to_s3(settings.aws_bucket_name,
-                      settings.aws_grid_file_name,
-                      population_grid,
+                      settings.aws_population_grid_file_name,
+                      sliced_dataset,
                       s3,
-metadata)
+                      metadata)
     
-    logger.info(f"Successfully updated dataset: {settings.aws_grid_file_name}")
+    logger.info(f"Successfully updated dataset: {settings.aws_population_grid_file_name}")
 
 else:
-    logger.info(f"Dataset {aws.grid_file_name} is up-to-date. Skipping download")
-
- 
+    logger.info(f"Dataset {aws.population_grid_file_name} is up-to-date. Skipping download")
