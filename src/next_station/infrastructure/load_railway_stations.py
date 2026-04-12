c93066e4e609d711caa6da2f9584e0e25360c32f
Luki <lukasjagusz6@gmail.com>
2026-04-12 19:13:23 +0200

    simplify path interface for load_json_source

--- src/next_station/infrastructure/load_railway_stations.py
+++ src/next_station/infrastructure/load_railway_stations.py
@@ -1,12 +1,8 @@
 from pyspark.sql import SparkSession, DataFrame
 
 def load_json_source(spark: SparkSession,
-                     path: tuple[str, str],
+                     aws_s3_path: str,
                      data_format: str = 'json'
                      ) -> DataFrame:
 
-    return (spark.read
-                .format(data_format)
-                .option('multiLine', 'true')
-                .load('/'.join(path)))
-
+    return spark.read.format(data_format).option('multiLine', 'true').load(aws_s3_path)
