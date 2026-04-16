e3fbfee3701f37819ba26f4b831591a496ae16c8
Luki <lukasjagusz6@gmail.com>
2026-04-16 15:51:29 +0200

    refactor: migrate main pipeline to nested settings namespaces

--- src/next_station/jobs/01_api_to_s3_railway_stations.py
+++ src/next_station/jobs/01_api_to_s3_railway_stations.py
@@ -4,10 +4,10 @@ from src.next_station.infrastructure.s3 import create_s3_client, upload_data_to_
 
 s3 = create_s3_client()
 
-train_stations = fetch_train_stations(settings.train_stations_url,
-                                      settings.query_for_train_stations)
+train_stations = fetch_train_stations(str(settings.api.base_railway_stations_url),
+                                      settings.api.payload_for_railway_stations)
 
-upload_data_to_s3(str(settings.absolute_railway_stations_path.parent),
-                  settings.absolute_railway_stations_path.name,
+upload_data_to_s3(settings.aws.s3_bucket_name,
+                  settings.aws.s3_railway_stations_file_name,
                   train_stations,
                   s3)
