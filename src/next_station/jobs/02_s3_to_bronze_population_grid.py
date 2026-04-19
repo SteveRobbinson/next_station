a59aff340d277cb4b3de1c3d46b67a624e35c0df
Luki <lukasjagusz6@gmail.com>
2026-04-19 11:25:08 +0200

    refactor: migrate static settings to pydantic models in population_grid job

--- src/next_station/jobs/02_s3_to_bronze_population_grid.py
+++ src/next_station/jobs/02_s3_to_bronze_population_grid.py
@@ -5,13 +5,13 @@ from src.next_station.infrastructure.databricks import save_df_in_db
 from sedona.spark import SedonaContext
 from src.next_station.quality.extract_population_grid import extract_population_points
 
-spark_session = get_spark_session(settings.databricks_python_user_name)
+spark_session = get_spark_session(settings.databricks.compute_config)
 
 spark_session = SedonaContext.create(spark_session)
 
 raw_df = load_population_grid(spark_session,
-                              settings.aws_population_grid_uri)
+                              settings.aws.population_grid_uri)
 
 df_exploded = extract_population_points(raw_df)
 
-save_df_in_db(df_exploded, settings.databricks_population_grid_table_id)
+save_df_in_db(df_exploded, settings.databricks.population_grid_fqn)
