47ea38e00755f8ded7289913012a9efa9b0bb77b
Luki <lukasjagusz6@gmail.com>
2026-04-24 14:53:51 +0200

    feat: implement multi-resolution H3 indexing for railway stations

--- dbt_project/models/silver/railway_stations/int_railway_stations_h3.sql
+++ dbt_project/models/silver/railway_stations/int_railway_stations_h3.sql
@@ -1,12 +1,6 @@
-with railway_stations as (
-  select * from {{ ref('stg_railway_stations') }}
-),
-
-railway_stations_h3 as (
-  select
-    *,
-    h3_longlatash3(latitude, longitude, {{ var('h3_resolution') }}) as h3_index
-  from railway_stations
-)
-
-select * from railway_stations_h3
+select
+  {% for h3_res in var('h3_resolutions') %}
+  h3_longlatash3(latitude, longitude, {{h3_res}}) as h3_index_{{h3_res}}
+  {%- if not loop.last %},{% endif %} 
+  {% endfor %}
+from {{ ref('stg_railway_stations') }}
