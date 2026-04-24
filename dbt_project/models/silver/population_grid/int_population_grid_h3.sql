11c84b500128dedbe82e239920c408cdc69cd967
Luki <lukasjagusz6@gmail.com>
2026-04-24 14:55:19 +0200

    feat: implement hierarchical H3 population sums using window functions

--- dbt_project/models/silver/population_grid/int_population_grid_h3.sql
+++ dbt_project/models/silver/population_grid/int_population_grid_h3.sql
@@ -1,6 +1,17 @@
+with h3_base as (
+  select
+    population_value,
+    {%- for h3_res in var('h3_resolutions') %}
+    h3_pointash3(geom_wkb, {{h3_res}}) as h3_index_{{h3_res}}
+    {%- if not loop.last %},{% endif %}
+    {% endfor %}
+  from {{ ref('stg_population_grid') }}
+)
+
 select
-  h3_pointash3(geom_wkb, {{ var('h3_resolution') }} as h3_index,
-  sum(population_value) as total_population
-from
-  {{ ref('stg_population_grid') }}
-group by 1
+  *,
+  {% for h3_res in var('h3_resolutions') %}
+  sum(population_value) over(partition by h3_index_{{h3_res}}) as total_population_{{h3_res}}
+  {%- if not loop.last %},{% endif %}
+  {% endfor %}
+from h3_base
