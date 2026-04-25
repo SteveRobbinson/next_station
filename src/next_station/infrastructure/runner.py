1b8c9b00ed8340ec4efbd4c64d826550af6d3949
Luki <lukasjagusz6@gmail.com>
2026-04-25 21:54:57 +0200

    refactor: simplify request retry logic using specialized exception adapters

--- src/next_station/infrastructure/runner.py
+++ src/next_station/infrastructure/runner.py
@@ -1,16 +1,9 @@
 import requests
 from requests.exceptions import HTTPError, Timeout, ConnectionError
 from src.next_station.core.config import settings
-from src.next_station.core.exceptions import (
-        ApiRequestError,
-        ApiUnauthorizedError,
-        ApiForbiddenRequestError,
-        ApiConnectionError,
-        ApiRateLimitError,
-        ApiUnhandledError,
-        ApiTimeoutError
-        )
 from src.next_station.infrastructure.utils import _perform_backoff
+from src.next_station.core.exceptions.external import APITimeoutError, APIResponseError
+from src.next_station.core.exceptions.base import UnifiedAPIError
 
 def runner(api_url: str,
            method: str,
@@ -27,62 +20,39 @@ def runner(api_url: str,
     if method not in settings.api.allowed_methods:
         raise(ValueError(f"Method {method} is not supported. Check allowed_methods in config."))
 
-
     if max_retries <= 0:
         raise ValueError("Max retries must be a positive integer")
 
-
     if payload:
-
         if method in ('GET', 'HEAD'):
             kwargs.setdefault('params', payload)
 
         elif method == 'POST':
             kwargs.setdefault('data', payload)
 
-
     for i in range(max_retries):
         
         try:
-                
             response = requests.request(method, url=api_url, allow_redirects=redirect, stream=stream, timeout=timeout, **kwargs)
-
             response.raise_for_status()
-
             return response
-            
-        except (Timeout, ConnectionError) as err:
 
+
+        except (Timeout, ConnectionError) as err:
             if i == max_retries - 1:
-                raise ApiTimeoutError(f"Final attempt {i+1} failed for {api_url}") from err
+                raise APITimeoutError(api_url, str(err)) from err
 
             _perform_backoff(i)
             continue
 
+
         except HTTPError as err:
-            
-            if response.status_code in (400, 404):
-                raise ApiRequestError(response) from err
+            status_code = err.response.status_code
 
-            elif response.status_code == 401:
-                raise ApiUnauthorizedError(response) from err
-
-            elif response.status_code == 403:
-                raise ApiForbiddenRequestError(response) from err
-
-            elif response.status_code in (429, 500, 501, 502, 503, 504):
-                if i == max_retries - 1:
-                    
-                    if response.status_code == 429:
-                        raise ApiRateLimitError(response) from err
-
-                    else:
-                        raise ApiConnectionError(response) from err
-
+            if status_code in [429, 500, 501, 502, 503, 504] and i < max_retries - 1:
                 _perform_backoff(i)
                 continue
 
-            else:
-                raise ApiUnhandledError(response) from err
+            raise APIResponseError(response) from err
 
-    raise RuntimeError("Unhandled error occured! Try again")
+    raise UnifiedAPIError(source = '### API ###', status_code = 500, details = 'Unhandled retry logic failure')
