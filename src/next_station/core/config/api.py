c2a14afc273c5f8264a50d32a4911b4fafd36d1e
Luki <lukasjagusz6@gmail.com>
2026-04-26 16:45:14 +0200

    refactor: implement specialized ApiRequestsConfig model

---
+++ src/next_station/core/config/api.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, HttpUrl
+
+class ApiRequestsConfig(BaseModel):
+    allowed_methods: set[str] = {'GET', 'HEAD', 'POST'}
+
+    base_railway_stations_url: HttpUrl
+    payload_for_railway_stations: str
+
+    base_population_grid_url: HttpUrl
+
