813b5a550bd29f11fe753edb5662b9e51b0383aa
Luki <lukasjagusz6@gmail.com>
2026-04-27 12:27:31 +0200

    fix: correct coordinate order in h3_longlatash3 function

--- dbt_project/models/silver/railway_stations/int_railway_stations_h3.sql
+++ dbt_project/models/silver/railway_stations/int_railway_stations_h3.sql
@@ -1,6 +1,6 @@
 select
   {% for h3_res in var('h3_resolutions') %}
-  h3_longlatash3(latitude, longitude, {{h3_res}}) as h3_index_{{h3_res}}
+  h3_longlatash3(longitude, latitude, {{h3_res}}) as h3_index_{{h3_res}}
   {%- if not loop.last %},{% endif %} 
   {% endfor %}
 from {{ ref('stg_railway_stations') }}
