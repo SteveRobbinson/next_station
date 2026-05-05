9cb10088fe83ef3eea8c55aa9cd507149ed1e23f
Luki <lukasjagusz6@gmail.com>
2026-05-05 15:42:35 +0200

    feat: implement standardized logging and API error handling

--- src/next_station/providers/fetch_population_grid.py
+++ src/next_station/providers/fetch_population_grid.py
@@ -1,6 +1,21 @@
-from next_station.infrastructure.runner import runner
+import logging
 import requests
+from next_station.core.exceptions.external import APIResponseError
+from next_station.infrastructure.runner import runner
+
+logger = logging.getLogger(__name__)
 
 def fetch_population_grid(api_url: str) -> requests.Response:
 
-    return runner(api_url, 'get', stream=True)
+    logger.info(f"Starting to fetch population grid dataset from {api_url}")
+    
+    try:
+        response = runner(api_url, 'get', stream=True)
+
+        logger.info(f"Successfully connected to population grid API. Status: {response.status_code}")
+        return response
+
+
+    except Exception as err:
+        logger.exception(f"Failed to fetch population grid from {api_url}")
+        raise APIResponseError(response) from err
--- src/next_station/providers/get_file_url.py
+++ src/next_station/providers/get_file_url.py
@@ -1,14 +1,27 @@
+import logging
 from next_station.infrastructure.runner import runner
 from next_station.schemas.worldpop import GetFileUrl
+from next_station.core.exceptions.external import APIResponseError
+
+logger = logging.getLogger(__name__)
 
 def get_file_url(api_url: str,
                  index: int = -1,
                  redirect = True
                  ) -> str:
+    
+    logger.info(f"Retrieving file_url from {api_url}")
 
-    response = runner(api_url, 'get', redirect=redirect)
-    response = response.json()
+    try:
 
-    result = GetFileUrl(**response)
+        response = runner(api_url, 'get', redirect=redirect)
+        response = response.json()
+        result = GetFileUrl(**response)
 
-    return result[index]
+        logger.info(f"Successfully retrieved file url from {api_url}")
+        return result[index]
+
+
+    except Exception as err:
+       logger.exception(f"Critical error during file URL retrieval from {api_url}")
+       raise APIResponseError(response) from err
